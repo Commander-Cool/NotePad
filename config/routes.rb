c9979f1774c7296cd32cc74633599c59704063a3
Jay Kumar <jakumar@blackberry.com>
2015-06-25 13:54:43 -0400

    Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
   root 'welcome#index'
+  authenticated :user do
+    root 'notes#index', as: "authenticated"
+  end
   resources :notes
 end
